2bee75a43393a9940786b343f537498cb4733e1d
Tabris05 <afiery1yt@gmail.com>
2024-02-21 16:46:44 -0800

    added basic node graph scene to confirm correctly installed library

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,26 @@
+#include <QtNodes/DataFlowGraphModel>
+#include <QtNodes/DataFlowGraphicsScene>
+#include <QtNodes/GraphicsView>
+#include <QtNodes/NodeDelegateModelRegistry>
 #include <QtWidgets/QApplication>
-#include <QWidget>
+#include <QtWidgets/QVBoxLayout>
+#include <QScreen>
 
-int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
+int main(int argc, char** argv) {
+    QApplication app{ argc, argv };
     QWidget window;
 
+    QtNodes::DataFlowGraphModel dataFlowGraphModel{ std::make_shared<QtNodes::NodeDelegateModelRegistry>() };
+    QtNodes::DataFlowGraphicsScene* scene = new QtNodes::DataFlowGraphicsScene{ dataFlowGraphModel, &window };
+    QtNodes::GraphicsView* view = new QtNodes::GraphicsView{ scene };
+
+    QVBoxLayout* layout = new QVBoxLayout{ &window };
+    layout->addWidget(view);
+    layout->setContentsMargins(0, 0, 0, 0);
+    layout->setSpacing(0);
+
+    window.setWindowTitle("DAWdle");
     window.showNormal();
+
     return app.exec();
 }
