daf39aa542b3d45e00af92b5a8aa8ff7baff3353
Tabris05 <afiery1yt@gmail.com>
2024-03-04 00:08:06 -0800

    refactor existing code, fixing mistakes along the way

--- src/arithmeticnode.cpp
+++ src/arithmeticnode.cpp
@@ -1,19 +1,24 @@
 #include "arithmeticnode.h"
 #include <QDebug>
 
-ArithmeticNode::ArithmeticNode() {
+ArithmeticNode::ArithmeticNode(const char* name) : m_name{name} {
     // Initialize buffer and result buffer
-    _buffer1 = std::make_shared<BufferData>();
-    _buffer2 = std::make_shared<BufferData>();
-    _resultBuffer = std::make_shared<BufferData>();
+    m_inBuffer1 = std::make_shared<BufferData>();
+    m_inBuffer2 = std::make_shared<BufferData>();
+    m_outBuffer = std::make_shared<BufferData>();
 
     // Assuming setAll is a function in BufferData to set all elements to a given value
-    _buffer1->setAll(0.0f);
-    _buffer2->setAll(0.0f);
-    _resultBuffer->setAll(0.0f);
+    m_inBuffer1->setAll(0.0f);
+    m_inBuffer2->setAll(0.0f);
+    m_outBuffer->setAll(0.0f);
 }
 
-ArithmeticNode::~ArithmeticNode() {
+QString ArithmeticNode::caption() const {
+    return m_name + QStringLiteral(" Node");
+}
+
+QString ArithmeticNode::name() const {
+    return m_name + QStringLiteral("Node");
 }
 
 bool ArithmeticNode::captionVisible() const {
@@ -33,24 +38,54 @@ QtNodes::NodeDataType ArithmeticNode::dataType(QtNodes::PortType, QtNodes::PortI
 void ArithmeticNode::setInData(std::shared_ptr<QtNodes::NodeData> data, QtNodes::PortIndex portIndex) {
     auto bufferData = std::dynamic_pointer_cast<BufferData>(data);
 
-    if (!bufferData) {
-        Q_EMIT dataInvalidated(portIndex);
-        return;
-    }
-
     if (portIndex == 0) {
-        _buffer1 = bufferData;
-    } else if (portIndex == 1) {
-        _buffer2 = bufferData;
+        for(size_t i = 0; i < BUFFERSIZE; i++) {
+            m_inBuffer1->m_buffer[i] = bufferData->m_buffer[i];
+        }
+    }
+    else if (portIndex == 1) {
+        for(size_t i = 0; i < BUFFERSIZE; i++) {
+            m_inBuffer2->m_buffer[i] = bufferData->m_buffer[i];
+        }
     }
 
     compute();
+    Q_EMIT dataUpdated(0);
 }
 
 std::shared_ptr<QtNodes::NodeData> ArithmeticNode::outData(QtNodes::PortIndex) {
-    return _resultBuffer;
+    return m_outBuffer;
 }
 
 QWidget* ArithmeticNode::embeddedWidget() {
     return nullptr;
+}
+
+AdditionNode::AdditionNode() : ArithmeticNode("Addition") {}
+SubtractionNode::SubtractionNode() : ArithmeticNode("Subtraction") {}
+MultiplicationNode::MultiplicationNode() : ArithmeticNode("Multiplication") {}
+DivisionNode::DivisionNode() : ArithmeticNode("Division") {}
+
+void AdditionNode::compute() {
+    for (int i = 0; i < BUFFERSIZE; i++){
+        m_outBuffer->m_buffer[i] = (m_inBuffer1->m_buffer[i] + m_inBuffer2->m_buffer[i]);
+    }
+}
+
+void SubtractionNode::compute() {
+    for (int i = 0; i < BUFFERSIZE; i++){
+        m_outBuffer->m_buffer[i] = (m_inBuffer1->m_buffer[i] + m_inBuffer2->m_buffer[i]);
+    }
+}
+
+void MultiplicationNode::compute() {
+    for (int i = 0; i < BUFFERSIZE; i++){
+        m_outBuffer->m_buffer[i] = (m_inBuffer1->m_buffer[i] + m_inBuffer2->m_buffer[i]);
+    }
+}
+
+void DivisionNode::compute() {
+    for (int i = 0; i < BUFFERSIZE; i++){
+        m_outBuffer->m_buffer[i] = (m_inBuffer1->m_buffer[i] + m_inBuffer2->m_buffer[i]);
+    }
 }
